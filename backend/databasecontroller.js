f6847d7d4d3db1b32c614603566514086f1c2d2c
JuanDT24 <tiradodj@uninorte.edu.co>
2024-10-18 23:40:56 -0500

    Added database connection

---
+++ backend/databasecontroller.js
@@ -0,0 +1,12 @@
+const {Client} = require('pg')
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432, 
+    password:"juan1234",
+    database:"labbases"
+})
+
+
+module.exports=client
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const path=require('path')
 const app=express();
+const client = require("../backend/databasecontroller.js")
 
 app.use(express.static(path.join(__dirname, '../frontend')));
 app.get('/', (req, res) => {
     res.sendFile(path.join('../frontend', 'index.html'));
 });
+client.connect();
+app.get('/continent', (req, res) => {
+    client.query(`Select distinct continent from covid_data;`, (err, dbRes) =>{
+    if(!err){
+        res.send(dbRes.rows);
+    }
+    });
+    client.end;
+})
 app.use((req, res) => {
     res.send('<h1> Error 404: Resource not found </h1>')
 });
