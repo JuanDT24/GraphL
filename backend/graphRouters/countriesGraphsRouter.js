a001d4195d3d6da949458543e44a12f72e83c0a4
JuanDT24 <tiradodj@uninorte.edu.co>
2024-10-25 00:03:25 -0500

    Adding changes to backup

--- backend/graphRouters/countriesGraphsRouter.js
+++ backend/graphRouters/countriesGraphsRouter.js
@@ -2,4 +2,7 @@ const express = require('express');
 const countriesGraphsRouter = express.Router();
 const client = require("../databasecontroller.js");
 
+
+countriesGraphsRouter.route('/:country/')
+
 module.exports = countriesGraphsRouter;
